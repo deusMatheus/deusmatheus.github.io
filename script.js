70b8dc2a752bf16ebcff0486bfd590f6ed5852eb
Matheus <matheus.pgua@hotmail.com>
2025-04-07 16:05:20 -0300

    Ajustes nos botões de idioma, agora estão funcionais. Ajustar os slides das imagens

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //let previousButton = document.getElementById('previous_service_button')
 //let nextButton = document.getElementById('next_service_button')
 
-const IDsLength = 3;
+const IDsLength = 4;
 var actualService = 1;
 var currentSlide = 0;
 var lastSlide = 0;
@@ -32,6 +32,9 @@ function slideShow() {
 }
 
 function getServiceDOM(actualService) {
+    if(document.querySelector('body').dataset.theme == 'enus'){
+        return document.getElementById('en-service-'+actualService);
+    }
     return document.getElementById('service-'+actualService);
 }
 
@@ -92,5 +95,34 @@ function previousButtonClick(){
         currentSlide = 1;
         lastSlide = 10;
         slideShow()
+    }
+}
+
+
+function setPTBR(){
+    const ptbr = document.getElementsByClassName('ptbr')
+    const enus = document.getElementsByClassName('enus')
+    document.querySelector('body').dataset.theme = 'ptbr';
+    actualService = 1;    
+    for (i=0; i < ptbr.length; i++){
+        ptbr[i].classList.remove('disabled');
+    }
+
+    for (i=0; i < enus.length; i++){
+        enus[i].classList.add('disabled');
+    }
+}
+
+function setENUS(){
+    const ptbr = document.getElementsByClassName('ptbr')
+    const enus = document.getElementsByClassName('enus')
+    document.querySelector('body').dataset.theme = 'enus';
+    actualService = 1;
+    for (i=0; i < ptbr.length; i++){
+        ptbr[i].classList.add('disabled');
+    }
+
+    for (i=0; i < enus.length; i++){
+        enus[i].classList.remove('disabled');
     }
 }
